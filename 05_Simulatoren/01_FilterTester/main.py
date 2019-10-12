f3c03de13412a73f480f8ebe739d6fda7217b719
Pluscrafter <pluscrafter123@hotmail.com>
2019-10-12 21:55:05 +0200

    add FFT and filtersim for quad old filtersim -> filtertester

--- 05_Simulatoren/01_FilterTester/main.py
+++ 05_Simulatoren/01_FilterTester/main.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import math
+import numpy as np
 
 # deltatime for plot
-dt = 0.000001
+dt = 0.0001
+# samplefreq
+fs = 1.0/dt
 
 
 # sine function with a dt of 0.000001 -> 1MHz "sample rate"
@@ -26,8 +29,8 @@ def digital_low_pass(cutoff_frequency, input):
 
 
 # number of sine periods
-period = 4
-period = period * 12500
+period = 10
+period = period * 125
 # x is the x axis value with steps of 0.000001 -> 1MHz "sample rate"
 x = []
 # sine80 is a list with numerical values for x sine period with the frequency of 80Hz
@@ -39,11 +42,11 @@ sinemixed = []
 # this loop calculate the values for the sine80 list
 for i in range(0, period):
     sine80.append(sine(1, 80, 0))
-    x.append(0.000001 * i)
+    x.append(dt * i)
 
 for i in range(0, period):
     sinemixed.append(sine(1, 80, 0) + sine(1, 133, math.pi) + sine(1, 800, 0.1) + sine(0.3, 1234, 50) +
-                     sine(0.5, 5010, 2.3) + sine(2, 80000, 3) + sine(0.5, 1000, 2.3) + sine(0.5, 1000000, 2.3))
+                     + sine(0.5, 1000, 2.3) + sine(0.5, 3000, 2.3))
 
 cutoffreq = 80
 filtered_dlpf = digital_low_pass(cutoffreq, sinemixed)
@@ -51,5 +54,21 @@ filtered_dlpf = digital_low_pass(cutoffreq, sinemixed)
 plt.plot(x, sinemixed, color='blue', linewidth=1, label='80Hz sine with noise')
 plt.plot(x, filtered_dlpf, color='red', linewidth=1, label='80Hz sine filtered with dlpf')
 plt.plot(x, sine80, color='green', linewidth=3, label='80Hz sine')
+plt.legend()
+plt.show()
+
+# https://www.cbcity.de/die-fft-mit-python-einfach-erklaert 12.10.19 18:23
+y1 = np.fft.fft(sinemixed)
+y2 = np.fft.fft(filtered_dlpf)
+y3 = np.fft.fft(sine80)
+
+N = int(len(y1)/2+1)
+
+X = np.linspace(0, fs/2, N, endpoint=True)
+
+
+plt.plot(X, np.abs(y1[:N]/N), color='blue', linewidth=1, label='80Hz sine with noise')
+plt.plot(X, np.abs(y3[:N]/N), color='green', linewidth=3, label='80Hz sine')
+plt.plot(X, np.abs(y2[:N]/N), color='red', linewidth=1, label='80Hz sine filtered with dlpf')
 plt.legend()
 plt.show()
---
+++ 05_Simulatoren/02_FilterSimulator/main.py
@@ -0,0 +1,57 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import math
+
+
+def digital_low_pass(cutoff_frequency, input):
+    # https://en.wikipedia.org/wiki/Low-pass_filter#Simple_infinite_impulse_response_filter
+    fc = 2 * math.pi * cutoff_frequency
+    alpha = (fc * dt) / (1 + fc * dt)
+    output = [0] * len(input)
+    output[0] = input[0]
+
+    print(alpha)
+
+    for i in range(1, len(input)):
+        output[i] = alpha * input[i] + (1 - alpha) * output[i - 1]
+    return output
+
+
+with open('LOG','r') as file:
+    line = []
+    for i in file:
+        line.append(i.rstrip('\n'))
+
+time = []
+dtime = []
+dt = 0
+
+pitch = []
+roll = []
+
+for i in line:
+    x = i.split('\t')
+    pitch.append(float(x[0]))
+    roll.append(float(x[1]))
+    time.append(float(x[3]))
+
+for i in range(1, len(time)):
+    dtime.append(time[i]-time[i-1])
+
+for i in dtime:
+    dt += i
+
+dt = dt/len(dtime)
+freq = 1.0/dt
+
+fpitch = digital_low_pass(80, pitch)
+froll = digital_low_pass(80, roll)
+
+print(dt)
+pitch = np.asarray(froll)
+y = np.fft.fft(froll)
+N = int(len(y)/2+1)
+X = np.linspace(0, freq/2, N, endpoint=True)
+
+plt.plot(X, np.abs(y[:N]/N))
+plt.show()
