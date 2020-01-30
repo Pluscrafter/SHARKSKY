6f51c93a382a2ebac80617f4661b1f86a4e485a7
Pluscrafter <pluscrafter123@hotmail.com>
2020-01-31 00:44:00 +0100

    change to old

--- 05_Simulatoren/04_PID_Arduino/PID_Arduino/PID_Arduino.ino
+++ 05_Simulatoren/04_PID_Arduino/PID_Arduino/PID_Arduino.ino
@@ -3,48 +3,27 @@
 Servo S;
 
 float sollwert = 90;
-float p,i,d,pp = 4,ii = 5,dd = 0.1;
+float p,i,d;
 float error = 0, preverror = 0;
 float delaytime = 50;
-
-String data = "";
-
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(9, OUTPUT);
   S.attach(9);
 }
 
 void loop() {
-  if(Serial.available() > 0){
-    char inChar = (char)Serial.read();
-    data += inChar;
-    if(inChar == '\n'){
-      String str(data);
-      char str_array[str.length()];
-      str.toCharArray(str_array, str.length());
-      char* token = strtok(str_array, " ");
-      pp = atof(token);
-      token = strtok(NULL, " ");
-      ii = atof(token);
-      token = strtok(NULL, " ");
-      dd = atof(token);
-      token = strtok(NULL, " ");
-      sollwert = atof(token);
-      data = "";
-    }
-  }
   int currentpos = map(analogRead(A2),813,10,0,180);
   S.attach(9);
   error = sollwert - currentpos;
-  
-  p = error * pp;//4
-  i += error * ii * ((delaytime)/1000); //0.01
-  d = dd * ((error - preverror)/((delaytime)/1000));//0.08
+  Serial.println(currentpos);
+  p = error * 4;//5
+  i += error * 0.01 * (delaytime/1000); //0.03
+  d = 0.08 * ((error - preverror)/(delaytime/1000));
 
   float pid = p + i + d;
   preverror = error;
-  Serial.println(currentpos);
+
   S.write(currentpos + pid);
   delay(delaytime/2);
 
